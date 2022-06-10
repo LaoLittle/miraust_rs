becbe932b4069971c7391b2840d8849d547796c5
LaoLittle <y1941803875@outlook.com>
2022-06-10 23:45:03 +0800

    remove unnecessary struct

--- miraust_core/src/contact/group.rs
+++
@@ -1,7 +0,0 @@
-use jni::objects::GlobalRef;
-
-use crate::contact::Contact;
-
-pub struct Group {
-    pub(crate) contact: Contact,
-}
--- miraust_core/src/contact/stranger.rs
+++
@@ -1,5 +0,0 @@
-use jni::objects::GlobalRef;
-
-pub struct Stranger {
-    pub(crate) inner: GlobalRef,
-}
