d31713aa2668c8f0a96aa462df452713a78bb663
LaoLittle <y1941803875@outlook.com>
2022-06-12 20:10:51 +0800

    cache jclass

--- miraust_core/src/jni_ffi/jni_loader.rs
+++ miraust_core/src/jni_ffi/jni_loader.rs
@@ -20,7 +20,7 @@ macro_rules! jni_method {
 // jni_onload 实现
 #[no_mangle]
 #[allow(non_snake_case)]
-extern "system" fn JNI_OnLoad(jvm: JavaVM, _reserved: *mut c_void) -> jint {
+extern "C" fn JNI_OnLoad(jvm: JavaVM, _reserved: *mut c_void) -> jint {
     let mut status: jint;
     // register to class RustPluginLoader
     let plugin_loader = "org/laolittle/loader/RustPluginLoader";
@@ -36,7 +36,6 @@ extern "system" fn JNI_OnLoad(jvm: JavaVM, _reserved: *mut c_void) -> jint {
     status = register_natives(&jvm, plugin_loader, &loader_methods);
     if status == JNI_ERR { return JNI_ERR; }
 
-    // for test now
     let class_name: &str = "org/laolittle/EventHandler";
     let jni_methods = [
         jni_method!("broadcast", "(Lnet/mamoe/mirai/event/Event;B)V", broadcast)
@@ -91,6 +90,7 @@ fn set_callback(jvm: JavaVM) {
 
         // bot
         let bot_class = env.find_class("net/mamoe/mirai/Bot")?;
+
         let bot_get_instance = env.get_static_method_id(bot_class, "findInstance", "(J)Lnet/mamoe/mirai/Bot;")?;
         let bot_get_friend = env.get_method_id(bot_class, "getFriend", "(J)Lnet/mamoe/mirai/contact/Friend;")?;
         let bot_get_group = env.get_method_id(bot_class, "getGroup", "(J)Lnet/mamoe/mirai/contact/Group;")?;
@@ -98,17 +98,36 @@ fn set_callback(jvm: JavaVM) {
 
         // message event
         let message_event_class = env.find_class("net/mamoe/mirai/event/events/MessageEvent")?;
+
         let message_event_get_subject = env.get_method_id(message_event_class, "getSubject", "()Lnet/mamoe/mirai/contact/Contact;")?;
         let message_event_get_message = env.get_method_id(message_event_class, "getMessage", "()Lnet/mamoe/mirai/message/data/MessageChain;")?;
 
         // message
         let message_class = env.find_class("net/mamoe/mirai/message/data/Message")?;
+
         let message_to_string = env.get_method_id(message_class, "toString", "()Ljava/lang/String;")?;
         let message_content_to_string = env.get_method_id(message_class, "contentToString", "()Ljava/lang/String;")?;
 
+        // plain_text
+        let plain_text_class = env.find_class("net/mamoe/mirai/message/data/PlainText")?;
+
+        let new_plain_text = env.get_method_id(plain_text_class, "<init>", "(Ljava/lang/String;)V")?;
+
         // contact
         let contact_class = env.find_class("net/mamoe/mirai/contact/Contact")?;
+
         let contact_send_message = env.get_method_id(contact_class, "sendMessage", "(Lnet/mamoe/mirai/message/data/Message;)Lnet/mamoe/mirai/message/MessageReceipt;")?;
+
+        // message_chain builder
+        let message_chain_builder_class = env.find_class("net/mamoe/mirai/message/data/MessageChainBuilder")?;
+
+        let new_message_chain_builder = env.get_method_id(message_chain_builder_class, "<init>", "(I)V")?;
+        let message_chain_builder_add = env.get_method_id(message_chain_builder_class, "add", "(Lnet/mamoe/mirai/message/data/Message;)Z")?;
+        let message_chain_builder_as_message_chain = env.get_method_id(message_chain_builder_class, "asMessageChain", "()Lnet/mamoe/mirai/message/data/MessageChain;")?;
+
+
+        let plain_text_class = env.new_global_ref(plain_text_class)?;
+        let message_chain_builder_class = env.new_global_ref(message_chain_builder_class)?;
 
         if MIRAI_ENV.set(MiraiEnv {
             jvm,
@@ -121,7 +140,13 @@ fn set_callback(jvm: JavaVM) {
             message_event_get_message,
             message_to_string,
             message_content_to_string,
+            plain_text_class,
+            new_plain_text,
             contact_send_message,
+            message_chain_builder_class,
+            new_message_chain_builder,
+            message_chain_builder_add,
+            message_chain_builder_as_message_chain,
         }).is_err() {
             env.throw_new("java/lang/RuntimeException", "Unable to set mirai_env")?;
         };
@@ -130,6 +155,7 @@ fn set_callback(jvm: JavaVM) {
     }
 
     if let Err(e) = set_callback_inner(jvm, env) {
+        env.exception_describe().unwrap();
         env.throw_new("java/lang/RuntimeException", format!("Unable to set callback: {e}")).expect("Unknown reason in set_callback::$set_callback_inner$");
     };
 }
